be6a7c7bcb372c34ff15bc7d78227f4cc2f2c51d
Vitali Schumarov <v.schumarov@icloud.com>
2025-02-03 05:22:48 +0100

    first commit

---
+++ src/fetchData.js
@@ -0,0 +1,25 @@
+export async function fetchWeatherData(url) {
+  showLoadingScreen();
+  let response = await fetch(url);
+  if (!response.status) {
+    return;
+  }
+  let data = await response.json();
+  deleteLoadingScreen();
+  return data;
+}
+
+function showLoadingScreen() {
+  console.log("loadingScreen");
+  document.querySelector(".currentWeather").style.display = "none";
+  const loadingText = "Loading...";
+  const loadingDiv = document.createElement("div");
+  loadingDiv.setAttribute("class", "loadingScreen");
+  loadingDiv.innerHTML = loadingText;
+  document.querySelector(".app").appendChild(loadingDiv);
+}
+
+function deleteLoadingScreen() {
+  document.querySelector(".loadingScreen").style.display = "none";
+  document.querySelector(".currentWeather").style.display = "flex";
+}
---
+++ src/main.js
@@ -0,0 +1,20 @@
+import "./style.scss";
+import { fetchWeatherData } from "./fetchData";
+
+let cityName = "Leverkusen";
+// https://www.weatherapi.com/docs/#
+const WEATHER_API = `http://api.weatherapi.com/v1/current.json?key=4d9509708acc49a6a8740155253101&q=${cityName}&lang=DE`;
+
+const cityNameEl = document.querySelector(".city");
+const weatherEl = document.querySelector(".weather");
+const conditionEl = document.querySelector(".condition");
+
+let data = await fetchWeatherData(WEATHER_API);
+
+function displayCityInformation(city) {
+  cityNameEl.innerHTML = city.location.name;
+  weatherEl.innerHTML = `${city.current.temp_c}°`;
+  conditionEl.innerHTML = city.current.condition.text;
+}
+
+displayCityInformation(data);
