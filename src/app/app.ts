7d129484e319e26b21aa260403189edf5ba73cac
Vitali <83905885+vitalischumarov@users.noreply.github.com>
2025-12-24 21:37:53 +0100

    main component created

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { NavComponent } from './Components/nav-component/nav-component';
+import { MainComponent } from './Components/main-component/main-component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavComponent],
+  imports: [NavComponent, MainComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
